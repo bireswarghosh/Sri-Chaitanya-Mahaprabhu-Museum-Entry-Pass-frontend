93a8ee6fdad948df66e439903ed5badd5e368657
bireswar <bireswarghosh770@gmail.com>
2025-10-24 12:58:06 +0530

    Remove basename from BrowserRouter for Vercel deployment

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import  ThemeContext  from "./context/ThemeContext";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/react/demo'>
+    <BrowserRouter>
       <ThemeContext>
         <App />
       </ThemeContext>
